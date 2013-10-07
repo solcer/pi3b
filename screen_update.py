4689d3b26f834d061de84982fd6ddd5a91701171
Kaan Akşit <kunguz@gmail.com>
2013-10-07 11:33:16 +0000

    Initial commit of Python script to display content on the screen via fbi

---
+++ screen_update.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = ('Kaan Akşit')
+
+import sys
+
+def anadongu():
+    print 'PI3B betiği,Yazar(lar): %s' % __author__
+    return True
+
+if __name__ == '__main__':
+    sys.exit(anadongu())
