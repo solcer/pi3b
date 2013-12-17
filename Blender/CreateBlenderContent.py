96fe40c126559c70adea75796c399de86e8e5292
Kaan Akşit <kunguz@gmail.com>
2013-12-17 13:05:28 +0200

    New mickey mouse content is created using Blender.

---
+++ Blender/CreateBlenderContent.py
@@ -0,0 +1,24 @@
+# Before running this scipt, open mickey.blend.
+# Vector comes with mathutils.
+import mathutils
+# Set Camera object to a variable.
+cam = bpy.dataobjects['Camera']
+
+# Loop to create images from different perspectives.
+for counter in range(0,18):
+    # Angle is calculated according to counter.
+    alpha = 3*counter
+    # Radius of the circle.
+    h     = -1.1
+    # Coordinates for the new camera location.
+    x     = sin(radians(-alpha))*h; y= cos(radians(alpha))*h; z= 0
+    # Set Camera location.
+    cam.location = (x,y,z)
+    # Camera rotation is set accordingly to new location.
+    cam.rotation_euler = (pi/2-radians(alpha),pi/2,0)
+    # Determines where to save image.
+    bpy.data.scenes['Scene'].render.filepath = '/home/kaan/image%s.jpg' % counter
+    # Renders image.
+    bpy.ops.render.render(write_still=True)
+    # Increment counter.
+    counter += 1
