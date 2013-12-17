f54cd97b2def7afb61029ed43cb4f28773603976
Kaan Akşit <kunguz@gmail.com>
2013-12-17 15:34:31 +0200

    Switch to correct camera angle, previously it was 90 degrees wrong.

--- Blender/CreateBlenderContent.py
+++ Blender/CreateBlenderContent.py
@@ -7,15 +7,15 @@ cam = bpy.dataobjects['Camera']
 # Loop to create images from different perspectives.
 for counter in range(0,18):
     # Angle is calculated according to counter.
-    alpha = 10*counter
+    alpha = -5*counter
     # Radius of the circle.
     h     = -1.1
     # Coordinates for the new camera location.
-    x     = sin(radians(-alpha))*h; y= cos(radians(alpha))*h; z= 0
+    x     = sin(radians(alpha))*h; y= cos(radians(alpha))*h; z= 0
     # Set Camera location.
     cam.location = (x,y,z)
     # Camera rotation is set accordingly to new location.
-    cam.rotation_euler = (pi/2-radians(alpha),pi/2,0)
+    cam.rotation_euler = (pi/2+radians(alpha),pi/2,0)
     # Determines where to save image.
     bpy.data.scenes['Scene'].render.filepath = '/home/kaan/image%s.jpg' % counter
     # Renders image.
